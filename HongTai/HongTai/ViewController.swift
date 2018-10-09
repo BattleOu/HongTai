0a0d53c0687e8a29cfc1bf43efb2c137b39827e5
BattleOu <43944760+BattleOu@users.noreply.github.com>
2018-10-09 18:35:27 +0800

    改了58

--- HongTai/HongTai/ViewController.swift
+++ HongTai/HongTai/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("2")
+        print("58")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
