bc5d8ac18a0551f98a81ff28ad27ccd0168a3684
欧张帆 <swiftou@ouzhangfandeimac.local>
2018-10-09 16:39:29 +0800

    上传了

---
+++ HongTai/HongTai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HongTai
+//
+//  Created by 欧张帆 on 2018/10/9.
+//  Copyright © 2018年 欧张帆. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ HongTai/HongTaiTests/HongTaiTests.swift
@@ -0,0 +1,34 @@
+//
+//  HongTaiTests.swift
+//  HongTaiTests
+//
+//  Created by 欧张帆 on 2018/10/9.
+//  Copyright © 2018年 欧张帆. All rights reserved.
+//
+
+import XCTest
+@testable import HongTai
+
+class HongTaiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
