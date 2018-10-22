6f8ee17a60fe21aaaddc306fab1fd5be2c0130ef
xuzhouxu <43941187+xuzhouxu@users.noreply.github.com>
2018-10-22 16:57:33 +0800

    check one

--- HongTai/HongTai/首页/Homepage.swift
+++ HongTai/HongTai/首页/Homepage.swift
@@ -1,23 +1,23 @@
 //
-//  ViewController.swift
+//  Homepage.swift
 //  HongTai
 //
-//  Created by 欧张帆 on 2018/10/9.
+//  Created by 周旭 on 2018/10/22.
 //  Copyright © 2018年 欧张帆. All rights reserved.
 //
 
 import UIKit
-
 import Alamofire
 import CoreData
 class Homepage: UIViewController,UISearchBarDelegate {
-    @IBOutlet weak var logining: UIButton!
+    
     var search:UISearchBar!
     var nameuser:String!
     var label:UILabel!
-    @IBOutlet weak var Tableview: UITableView!
     var god = [goodgood]()
     var godd:goodgood?
+    @IBOutlet weak var logingin: UIButton!
+    @IBOutlet weak var Tableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         search = UISearchBar(frame:CGRect(x: 60, y:30, width:220, height:40))
@@ -47,30 +47,30 @@ class Homepage: UIViewController,UISearchBarDelegate {
         if (nameuser == nil)
         {
             logining.isHidden = false
-//        var button:UIButton = UIButton(type:.contactAdd)
-//        button = UIButton(frame: CGRect(x: 300, y: 40, width: 50, height: 20))
-////        label.titleLabel?.textColor = UIColor.gray
-////        label.titleLabel?.text = "登录/"
-//        button.setTitle("登录/", for: .normal)
-//        button.setTitleColor(UIColor.gray, for: .normal)
-//        button.titleLabel?.font = UIFont(name:"Arial-BoldItalicMT", size:15)
-//        button.addTarget(self,action:#selector(methodName), for: .touchUpInside)
-//         view.addSubview(button)
-////        lab = UIButton(frame: CGRect(x: 345, y: 40, width: 40, height: 20))
-////        lab.titleLabel?.textColor = UIColor.gray
-////        lab.titleLabel?.text = "注册"
-////        lab.titleLabel?.font = UIFont(name:"Zapfino", size:15)
-////         view.addSubview(lab)
-//
-//        var zhucebutton:UIButton = UIButton(type:.contactAdd)
-//        zhucebutton = UIButton(frame: CGRect(x: 350, y: 40, width: 40, height: 20))
-//        //        label.titleLabel?.textColor = UIColor.gray
-//        //        label.titleLabel?.text = "登录/"
-//        zhucebutton.setTitle("注册", for: .normal)
-//        zhucebutton.setTitleColor(UIColor.gray, for: .normal)
-//        zhucebutton.titleLabel?.font = UIFont(name:"Arial-BoldItalicMT", size:15)
-//        button.addTarget(self,action:#selector(someMethod), for: .touchUpInside)
-//        view.addSubview(zhucebutton)
+            //        var button:UIButton = UIButton(type:.contactAdd)
+            //        button = UIButton(frame: CGRect(x: 300, y: 40, width: 50, height: 20))
+            ////        label.titleLabel?.textColor = UIColor.gray
+            ////        label.titleLabel?.text = "登录/"
+            //        button.setTitle("登录/", for: .normal)
+            //        button.setTitleColor(UIColor.gray, for: .normal)
+            //        button.titleLabel?.font = UIFont(name:"Arial-BoldItalicMT", size:15)
+            //        button.addTarget(self,action:#selector(methodName), for: .touchUpInside)
+            //         view.addSubview(button)
+            ////        lab = UIButton(frame: CGRect(x: 345, y: 40, width: 40, height: 20))
+            ////        lab.titleLabel?.textColor = UIColor.gray
+            ////        lab.titleLabel?.text = "注册"
+            ////        lab.titleLabel?.font = UIFont(name:"Zapfino", size:15)
+            ////         view.addSubview(lab)
+            //
+            //        var zhucebutton:UIButton = UIButton(type:.contactAdd)
+            //        zhucebutton = UIButton(frame: CGRect(x: 350, y: 40, width: 40, height: 20))
+            //        //        label.titleLabel?.textColor = UIColor.gray
+            //        //        label.titleLabel?.text = "登录/"
+            //        zhucebutton.setTitle("注册", for: .normal)
+            //        zhucebutton.setTitleColor(UIColor.gray, for: .normal)
+            //        zhucebutton.titleLabel?.font = UIFont(name:"Arial-BoldItalicMT", size:15)
+            //        button.addTarget(self,action:#selector(someMethod), for: .touchUpInside)
+            //        view.addSubview(zhucebutton)
         }
         else
         {
@@ -85,25 +85,24 @@ class Homepage: UIViewController,UISearchBarDelegate {
             logining.setTitle("注销", for: .normal)
             logining.titleLabel?.font = UIFont(name:"Arial-BoldItalicMT", size:12)
         }
+        // Do any additional setup after loading the view.
     }
     
-//    @objc func methodName() {
-//         let controller = self.storyboard?.instantiateViewController(withIdentifier: String(describing: type(of: LoginViewController()))) as! LoginViewController
-//        self.present(controller, animated: true)
-//    }
-//    @objc func someMethod() {
-//        let controller = self.storyboard?.instantiateViewController(withIdentifier: String(describing: type(of: RegisterViewController()))) as! RegisterViewController
-//        self.present(controller, animated: true)
-//    }
-    
-    @IBAction func login(_ sender: Any) {
+
+    @IBAction func lgoing(_ sender: Any) {
         self.performSegue(withIdentifier: "login",sender:self);
     }
-    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
-//遵循协议代理，调用代理方法
 extension Homepage : JHLoopViewDelegate {
     func adLoopView(_ adLoopView: JHLoopView, IconClick index: NSInteger) {
         print(index)
--- HongTai/HongTai/首页/TableViewCell.swift
+++ HongTai/HongTai/首页/TableViewCell.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
-    @IBOutlet weak var lblgood: UILabel!
     @IBOutlet weak var picture: UIImageView!
+    
+    @IBOutlet weak var lblgood: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         picture.layer.cornerRadius = min(picture.bounds.size.width, picture.bounds.size.height) * 1
