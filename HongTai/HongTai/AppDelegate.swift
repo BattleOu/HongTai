375230aa6afa65546eb4319c785e8c3bc3d85b6e
BattleOu <43944760+BattleOu@users.noreply.github.com>
2018-11-19 17:23:49 +0800

    启动屏修改和设置时间

--- HongTai/HongTai/AppDelegate.swift
+++ HongTai/HongTai/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //设置启动图片停留时间
+        Thread .sleep(forTimeInterval: 5.0)
         return true
     }
 
